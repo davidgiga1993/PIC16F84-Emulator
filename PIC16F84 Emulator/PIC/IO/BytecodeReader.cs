1a2ad60038caadca4ab7d763ba42a4f647c49b05
David <davidgiga1993@gmail.com>
2014-03-31 12:22:21 +0200

    Started Bytecode reader

---
+++ PIC16F84 Emulator/PIC/IO/BytecodeReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace PIC16F84_Emulator.PIC.IO
+{
+    public class BytecodeReader
+    {
+        public byte[] ReadSourcecode(string Filepath)
+        {
+            //FileStream fs = new FileStream();
+            return null;
+        }
+    }
+}
